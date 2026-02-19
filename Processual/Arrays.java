5e3198d7339158f2191ded882c4a144db5ed72dd
saymonoliver <saymonoliveira.sousa@gmail.com>
2026-02-19 19:59:05 -0300

    Create Arrays.java

---
+++ Processual/Arrays.java
@@ -0,0 +1,14 @@
+Os Arrays Unidimensionais são os que possuem apenas um índice para acessar seu conteúdo eles são declarados da seguinte forma:
+
+Tipo de dado -> nome do array = new tipo-de-dado [quantidade] 
+Tipo de dado [] -> pode ser qualquer tipo de variável 
+Nome do array -> um nome válido; as mesmas regras para nomes de variáveis
+Quantidade -> quantidade de elementos que o array pode manipular.
+
+Exemplos:
+
+INT [] numeros = new int[10]; cria um array com o nome numeros que contém 10 elemento do tipo int e seu índice varia de 0 a 9
+DOUBLE [] precos = new double [5]; cria um array com o nome precos que contém 5 elementos do tipo double e seu índice varia de 0 a 4
+CHAR [] sexo = new char [2]; cria um array com o nome sexo que contém 2 elementos do tipo char e seu índice varia entre 0 e 1
+BOOLEAN [] opcoes  = new boolean [2]; cria um array com o nome opcoes que contém 2 elementos do tipo boolean e seu índice varia entre 0 e 1
+STRING [] meses  = new string [12]; cria um array com o nome meses que contém 12 elementos do tipo string e e seu índice varia de 0 a 11
